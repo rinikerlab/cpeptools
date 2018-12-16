09d0443f36772af918ffad37e6535e8cd1a89065
hjuinj <shuzhe.wang1108@gmail.com>
2018-12-16 17:40:02 +0100

    minor fixs to support CI

--- cpeptools/tests/test_confgen.py
+++ cpeptools/tests/test_confgen.py
@@ -6,6 +6,6 @@ def test_confgen():
     CsA_smiles = "CC[C@H]1C(=O)N(CC(=O)N([C@H](C(=O)N[C@H](C(=O)N([C@H](C(=O)N[C@H](C(=O)N[C@@H](C(=O)N([C@H](C(=O)N([C@H](C(=O)N([C@H](C(=O)N([C@H](C(=O)N1)[C@@H]([C@H](C)C/C=C/C)O)C)C(C)C)C)CC(C)C)C)CC(C)C)C)C)C)CC(C)C)C)C(C)C)CC(C)C)C)C"
     CsA = Chem.MolFromSmiles(CsA_smiles, sanitize = True)
     ref = CsA
-    m = Chem.MolFromPDBFile('/home/shuwang/Documents/Modelling/CP/Datum/macrocycles_benchmark/csa_reference.pdb', removeHs = False, sanitize = True)
+    m = Chem.MolFromPDBFile("cpeptools/tests/data/CsA.pdb", removeHs = False, sanitize = True)
     CsA = AllChem.AssignBondOrdersFromTemplate(ref, m)
     assert generate_conformers_with_eccentricity(CsA, 1, 0)
